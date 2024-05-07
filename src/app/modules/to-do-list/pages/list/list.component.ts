d82f83a709d0a52498c42abd359747a45d86e460
desafiogamer <joaopap1234@gmail.com>
2024-05-07 16:24:33 -0300

    atualização

---
+++ src/app/modules/to-do-list/pages/list/list.component.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+  public addItem = signal(true)
+}
